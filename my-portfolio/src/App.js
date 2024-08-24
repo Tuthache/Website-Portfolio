74407f595be0a54cbd4295c660829f6daeaa9a48
Tuthache <64441477+Tuthache@users.noreply.github.com>
2024-08-23 17:55:08 -0700

    Added in routes for files

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Hobbies from "./pages/Hobbies";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/hobbies/*" element={<Hobbies />} />
       </Routes>
     </Router>
   );
---
+++ my-portfolio/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="">
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default About;
